459767d48d778c2e7c2d8ee32cc3b2d7e2d97b75
HACKERZKALI <62069925+HACKERZKALI@users.noreply.github.com>
2020-03-15 17:29:37 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,8 +11,8 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "   "
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("I'?M ALIVE...... JINDA HU SIR\n\n"
+    await alive.edit("I'M ALIVE...... JINDA HU SIR\n\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\nFork by:` @TECHNICALHACKERZ\n"
-                     "`Bot created by:` [SnapDragon](tg://user?id=719877937)\n"
+                     "`Bot created by:` [HACKERZKALI](tg://user?id=1106906182)\n"
                      "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     "[This is My Code](https://github.com/hackerzkali/user-bot)") 
+                     "[Deploy Code](https://heroku.com/deploy?template=https://github.com/HACKERZKALI/user-bot)") 
